66ff1808681c35af431f79a948d25b9e6fa3f6f3
Nitin Bhandari <nitin211206@gmail.com>
2025-07-05 19:51:13 +0530

    fix: Removed Bloat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { ModeToggle } from "../components/mode-toggle";
-
 export default function Home() {
   return (
     <div className="m-4">
