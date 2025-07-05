c0b155a18918d93b9d5419e8953619e0f81535e5
Nitin Bhandari <nitin211206@gmail.com>
2025-07-05 15:57:21 +0530

    fix: Double Render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,6 @@ import { ModeToggle } from "../components/mode-toggle";
 export default function Home() {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton mode='modal'>
-          <Button variant="outline">Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <Button variant="secondary">Click Me</Button>
-      <ModeToggle />
-    </div>          
+    </div>
   );
 }
